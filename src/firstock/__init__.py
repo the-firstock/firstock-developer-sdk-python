0dedc6f3217121a40c7e6e261b07b6abce24d9f5
Loukya Harisha <loukya.harisha@gmail.com>
2025-11-30 17:51:23 +0530

    changes

---
+++ src/firstock/__init__.py
@@ -0,0 +1,4 @@
+from .websockets import Firstock, FirstockWebSocket
+
+__version__ = "1.0.6"
+__all__ = ["Firstock", "FirstockWebSocket"]
---
+++ src/firstock/websockets/__init__.py
@@ -0,0 +1,4 @@
+from .websockets import Firstock, FirstockWebSocket
+from . import websocket_functions
+
+__all__ = ["Firstock", "FirstockWebSocket", "websocket_functions"]
