a9d3e02b2035ade79ebb3c4b3490b154620e4346
Jayashankar RJ <120076432+eviljay4@users.noreply.github.com>
2026-03-23 10:38:04 +0530

    changed the request body for modify amo

--- src/firstock/ordersNReport/placeAMOFunctionality/execution.py
+++ src/firstock/ordersNReport/placeAMOFunctionality/execution.py
@@ -1,24 +1,19 @@
-from firstock.ordersNReport.placeAMOFunctionality.functions import *
+from firstock.ordersNReport.modifyAMOFunctionality.functions import *
 
-class FirstockPlaceAMO:
-    def __init__(self, exch, tsym, qty, prc, prd, trantype, prctyp, ret, trgprc, remarks, userId):
-        self.placeAMO = ApiRequests()
-        self.exch = exch
-        self.tsym = tsym
+class FirstockModifyAMO:
+    def __init__(self, orderNumber, qty, prc, prctyp, prd, trgprc, userId):
+        self.modifyAMO = ApiRequests()
+        self.orderNumber = orderNumber
         self.qty = qty
         self.prc = prc
-        self.prd = prd
-        self.trantype = trantype
         self.prctyp = prctyp
-        self.ret = ret
+        self.prd = prd
         self.trgprc = trgprc
-        self.remarks = remarks
         self.userId = userId
 
-    def firstockPlaceAMO(self):
-        result = self.placeAMO.firstockPlaceAMO(
-            self.exch, self.tsym, self.qty, self.prc, self.prd,
-            self.trantype, self.prctyp, self.ret, self.trgprc,
-            self.remarks, self.userId
+    def firstockModifyAMO(self):
+        result = self.modifyAMO.firstockModifyAMO(
+            self.orderNumber, self.qty, self.prc,
+            self.prctyp, self.prd, self.trgprc, self.userId
         )
         return result
